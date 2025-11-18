0f63d9a22e42daab756f9141d0a038bb3039916c
jaroec <96500713+jaroec@users.noreply.github.com>
2025-11-18 13:46:35 -0400

    Delete backend/src/routes/customers.service.js

--- backend/src/routes/customers.service.js
+++
@@ -1,9 +0,0 @@
-// src/services/customers.service.js
-const Customers = require('../models/customers.model');
-
-module.exports = {
-  getAll: () => Customers.getAll(),
-  findById: (id) => Customers.findById(id),
-  create: (c) => Customers.create(c),
-  update: (id, fields) => Customers.update(id, fields)
-};
